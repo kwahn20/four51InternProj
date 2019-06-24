e5c611933b2cfa21255ef3a59c9e7008f6485cea
Kevin <kahn@four51.com>
2019-06-24 09:01:10 -0500

    First Commit w Files

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../../Themes/App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
